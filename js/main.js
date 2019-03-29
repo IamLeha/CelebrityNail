0f57e9e220fc94eb6849e1892a0c055a49f4701a
Дмитрий <dimonchik_dyatchin6464@mail.ru>
2019-03-29 13:14:26 +0300

    footer

---
+++ js/main.js
@@ -0,0 +1,55 @@
+$(".gallery").fancybox({
+    openEffect: 'elastic'
+});
+
+function Clear(){
+    document.getElementById('Myform').reset();
+    document.getElementById('Myform1').reset();
+}
+
+
+
+$('a[href^="#adress"]').bind('click.smoothscroll', function (e) {
+    e.preventDefault();
+
+    document.getElementById('burger').checked = false;
+
+    var target = this.hash,
+        $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 500, 'swing', function () {
+        window.location.hash = target;
+    });
+});
+
+$('a[href^="#bonuses"]').bind('click.smoothscroll', function (e) {
+    e.preventDefault();
+
+    document.getElementById('burger').checked = false;
+
+    var target = this.hash,
+        $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 500, 'swing', function () {
+        window.location.hash = target;
+    });
+});
+
+$('a[href^="#instagram"]').bind('click.smoothscroll', function (e) {
+    e.preventDefault();
+
+    document.getElementById('burger').checked = false;
+
+    var target = this.hash,
+        $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 500, 'swing', function () {
+        window.location.hash = target;
+    });
+});
